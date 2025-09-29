61559336208165954844bf9e54a0b965b820918d
Firdevs Akbayır <firdevszsa@gmail.com>
2025-09-29 18:23:17 +0300

    Update services.py

--- app/services.py
+++ app/services.py
@@ -1,16 +1,13 @@
 import string
 import random
-from app.database import url_db  # veritabani gibi davranan dict
+from app.database import url_db
 
-#kisa kod uretici
 def generate_short_code(length=6):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-# url kaydetme
 def save_url(short_code, long_url):
     url_db[short_code] = long_url
 
-# url getirme (yonlendirme icin)
 def get_url(short_code):
     return url_db.get(short_code)
 
